7e7899905c6ae08d477c464f08c23fe1c63ad765
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-02-21 21:11:50 +0200

    Update Styles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 const Hero = () => {
   function handleScroll() {}
   return (
-    <div className="hero">
+    <div className="hero align-center">
       <div className="flex-1 pt-36 padding-x ">
         <h1 className="hero__title">
           Find, book, or rent a car — quickly and easily
@@ -25,8 +25,8 @@ const Hero = () => {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
-          <div className="hero__image-overlay" />
         </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );

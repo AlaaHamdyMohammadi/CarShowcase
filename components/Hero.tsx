ce82da25f1fe694bc9a2ab915b05247e8228d027
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-02-22 19:38:24 +0200

    Update footer section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,18 +7,20 @@ import Image from "next/image";
 const Hero = () => {
   function handleScroll() {}
   return (
-    <div className="hero align-center">
-      <div className="flex-1 pt-36 padding-x ">
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, or rent a car — quickly and easily
+          Find, book, rent a car—quick and super easy!
         </h1>
+
         <p className="hero__subtitle">
           Streamline your car rental experience with our effortless booking
           process.
         </p>
+
         <CustomButton
           title="Explore Cars"
-          containerStyles="bg-primary-blue text-white rounded-full mt-10 mt-10"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
       </div>
@@ -26,6 +28,7 @@ const Hero = () => {
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+
         <div className="hero__image-overlay" />
       </div>
     </div>

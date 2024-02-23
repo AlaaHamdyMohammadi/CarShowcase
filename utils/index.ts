53c386c7965ce85d58ce1b1c97311360fd60ce51
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-02-23 20:18:22 +0200

    Add Rapid API

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,11 @@
+const api = process.env.API;
 export async function fetchCars() {
   const headers = {
     "X-RapidAPI-Key": "e78c112fc1msh1882f5f2dac387bp1f3632jsn274afd03621a",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
-  const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
-    { headers }
-  );
+  const response = await fetch( api , { headers });
 
   const data = await response.json();
 

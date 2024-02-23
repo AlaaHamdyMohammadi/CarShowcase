d418081c51a6d857c0744aea40971b59a828da4d
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-02-23 20:31:56 +0200

    Add Rapid API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,45 @@ import Image from "next/image";
 import Hero from "./../../components/Hero";
 import SearchBar from "../../components/SearchBar";
 import CustomFilter from "../../components/CustomFilter";
+import CarCard from "../../components/CarCard";
 import { fetchCars } from "../../utils";
 
- export default async function Home() {
-   const allCars = await fetchCars();
-   console.log(allCars);
-   return (
-     <main className="overflow-hidden">
-       <Hero />
-       <div className="mt-12 padding-x padding-y max-width" id="discover">
-         <div className="home__text-container">
-           <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
-           <p>Explore the cars you might like</p>
-         </div>
-         <div className="home__filters">
-           <SearchBar />
-           <div className="home__filter-container">
-             <CustomFilter title="fuel" />
-             <CustomFilter title="year" />
-           </div>
-         </div>
-       </div>
-     </main>
-   );
- }
+export default async function Home() {
+  const allCars = await fetchCars();
+  console.log(allCars);
+  const isDataEmpty = !allCars || allCars.length < 1 || !Array.isArray(allCars);
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard key={car.make} car={car}/>
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">
+              Oops, no cars found
+            </h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+}

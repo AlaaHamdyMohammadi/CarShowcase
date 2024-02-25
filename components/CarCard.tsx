959ff842b96bfd4ce1af8654701bd696145f9d11
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-02-25 20:33:43 +0200

    Implement car details

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { CarProps } from "../types";
 import { calculateCarRent } from "../utils";
 import CustomButton from "./CustomButton";
+import CarDetails from "./CarDetails";
 interface CarCardProps {
   car: CarProps;
 }
@@ -80,6 +81,9 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
+      
+        <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car}/>
+      
     </div>
   );
 };

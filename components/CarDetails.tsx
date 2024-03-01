c5c488269dbabbd7675d451982f88986ff5b575b
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-01 21:49:23 +0200

    Update style

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -2,6 +2,7 @@
 import React, { Fragment } from "react";
 import { CarProps } from "../types";
 import { Dialog, Transition } from "@headlessui/react";
+import Image from "next/image";
 interface CarDetailsProps {
   isOpen: boolean;
   closeModal: () => void;
@@ -35,7 +36,21 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <div className="fixed inset-0 bg-black bg-opacity-25" />
+                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col gap-5">
+                  <button
+                    type="button"
+                    onClick={closeModal}
+                    className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
+                  >
+                    <Image
+                      src="/close.svg"
+                      alt="close"
+                      width={20}
+                      height={20}
+                      className="object-contain"
+                    />
+                  </button>
+                </Dialog.Panel>
               </Transition.Child>
             </div>
           </div>
